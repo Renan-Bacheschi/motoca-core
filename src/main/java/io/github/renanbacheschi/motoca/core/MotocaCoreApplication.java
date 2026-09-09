760210a8f2b6733f1ce83f1d444958bd47fa334b
Renan Bacheschi Mafra <renanbacheschimafra@gmail.com>
2026-09-08 23:34:55 -0300

    chore: initialize Spring Boot project

---
+++ src/main/java/io/github/renanbacheschi/motoca/core/MotocaCoreApplication.java
@@ -0,0 +1,13 @@
+package io.github.renanbacheschi.motoca.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MotocaCoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MotocaCoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/github/renanbacheschi/motoca/core/MotocaCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.renanbacheschi.motoca.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MotocaCoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
